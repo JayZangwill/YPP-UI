0c45a6bf7a9678c9acf5a84cea6facf7f3e9a24b
caominjie <caominjie@tuhu.cn>
2018-08-21 14:42:25 +0800

    update

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,15 +1,19 @@
 import { VueConstructor } from 'vue'
 
-import YPPAlert from '@/lib/packages/alert/index.ts'
-import YPPCaptcha from '@/lib/packages/captcha/index.ts'
-import YPPConfirm from '@/lib/packages/confirm/index.ts'
-import YPPLoader from '@/lib/packages/loader/index.ts'
-import YPPProcess from '@/lib/packages/process/index.ts'
+import Alert from '@/lib/packages/alert/index.ts'
+import Captcha from '@/lib/packages/captcha/index.ts'
+import Confirm from '@/lib/packages/confirm/index.ts'
+import Loader from '@/lib/packages/loader/index.ts'
+import Process from '@/lib/packages/process/index.ts'
+
+const component = [
+  Alert,
+  Captcha,
+  Confirm,
+  Loader,
+  Process
+]
 
 export default function install(Vue: VueConstructor, options: object) {
-  Vue.use(YPPAlert)
-  Vue.use(YPPCaptcha)
-  Vue.use(YPPConfirm)
-  Vue.use(YPPLoader)
-  Vue.use(YPPProcess)
+  component.forEach(component => Vue.use(component))
 }
--- src/lib/typings/module-augmentation.d.ts
+++ src/lib/typings/module-augmentation.d.ts
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 
+import { LoaderServerEntity } from './loader'
 declare module 'vue/types/vue' {
   interface Vue {
     $alert: Function
     $confirm: Function
-    $loader: Object
+    $loader: LoaderServerEntity
   }
 }
