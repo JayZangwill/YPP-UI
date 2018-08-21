584f89092588615440b4eb35d7b3b6601c8dc57a
caominjie <caominjie@tuhu.cn>
2018-08-21 14:26:57 +0800

    save

---
+++ src/lib/packages/process/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <span class="ypp-process" :style="{width: width, height: height, backgroundColor: baseColor}">
+    <em class="ypp-process-inset" :style="{width: percent + '%', backgroundColor: insetColor}"></em>
+  </span>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+@Component
+export default class YPPProcess extends Vue {
+  @Prop({type: Number, default: 100}) percent!: number
+  @Prop({type: String, default: '4rem'}) width!: string
+  @Prop({type: String, default: '.2rem'}) height!: string
+  @Prop({type: String, default: '#f00'}) baseColor!: string
+  @Prop({type: String, default: '#00f'}) insetColor!: string
+}
+</script>
+
+<style lang="scss" scoped>
+.ypp-process{
+  position: relative;
+  display: inline-block;
+  border-radius: .4rem;
+  background-color: #f00;
+}
+.ypp-process-inset{
+  position: absolute;
+  left: 0;
+  height: 100%;
+  border-radius: .4rem;
+  background-color: #0ff;
+}
+</style>
--- src/lib/typings/module-augmentation.d.ts
+++ src/lib/typings/module-augmentation.d.ts
@@ -3,5 +3,7 @@ import Vue from 'vue'
 declare module 'vue/types/vue' {
   interface Vue {
     $alert: Function
+    $confirm: Function
+    $loader: Object
   }
 }
