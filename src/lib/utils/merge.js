c76afb2830d7240f3519c99ec333784d362dd20d
christian <baozaodexianyu@163.com>
2018-08-21 17:32:13 +0800

    merge.js

--- src/lib/utils/merge.js
+++ src/lib/utils/merge.js
@@ -1,9 +1,13 @@
 export default function(target) {
   for (let i = 1, j = arguments.length; i < j; i++) {
     let source = arguments[i] || {}
+
+    if (JSON.stringify(source) === '{}') break
+    
     for (let prop in source) {
       if (source.hasOwnProperty(prop)) {
         let value = source[prop]
+
         if (value !== undefined) {
           target[prop] = value
         }
