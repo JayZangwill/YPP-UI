a414a776f3b82a23df9084d72e0258880866b062
christian <baozaodexianyu@163.com>
2018-08-23 10:41:57 +0800

    inject on demand ’s solution move 2 injection.js

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,7 +2,7 @@ import { VueConstructor } from 'vue'
 
 import components from '@/lib/injection'
 
-import { reduceInitObject } from './typings/index.d'
+import * as SFCComponent from './injection'
 
 const install = function (Vue: VueConstructor) {
   // if (install.installed) return
@@ -13,25 +13,17 @@ if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue)
 }
 
-const tranformMap = components.reduce((initial: any, v: reduceInitObject<string, any>) => {
-  initial[v.name] = v.component
-  return initial
-}, {})
+export const Alert = SFCComponent.Alert
 
+export const Captcha = SFCComponent.Captcha
 
-// why no dynamic export ... ugly
+export const Confirm = SFCComponent.Confirm
 
-export const Alert: Function = tranformMap.Alert
+export const Loader = SFCComponent.Loader
 
-export const Captcha: Function = tranformMap.Captcha
+export const Process = SFCComponent.Process
 
-export const Confirm: Function = tranformMap.Confirm
-
-export const Loader: Function = tranformMap.Loader
-
-export const Process: Function = tranformMap.Process
-
-export const Toast: Function = tranformMap.Toast
+export const Toast = SFCComponent.Toast
 
 export default {
   install,
