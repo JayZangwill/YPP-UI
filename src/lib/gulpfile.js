2570b0a963df041247a9bb3fce6e60f473fceea6
caominjie <caominjie@tuhu.cn>
2018-08-23 13:47:45 +0800

    save

--- src/lib/gulpfile.js
+++ src/lib/gulpfile.js
@@ -7,28 +7,90 @@ const through2 = require('through2')
 const rimraf = require('rimraf')
 
 const gulp = require('gulp')
-const eslint = require('gulp-eslint')
-const babel = require('gulp-babel')
+// const eslint = require('gulp-eslint')
+// const babel = require('gulp-babel')
 const ts = require('gulp-typescript')
+
+const webpack = require('webpack')
 
 const cwd = process.cwd()
 
-console.log(cwd)
 gulp.task(
-  'vue',
+  'copy',
   () => {
-    return gulp.src(['./packages/**/*.ts', './utils/*.ts', '*.ts'])
+    return gulp.src(['./typings/*.ts'])
+      .pipe(
+        gulp.dest(process.cwd().replace(cwd, `${cwd}/dist/typings`))
+      )
+  }
+)
+
+gulp.task(
+  'copyvue',
+  () => {
+    return gulp.src(['./packages/**/*.vue'])
+      .pipe(
+        gulp.dest(process.cwd().replace(cwd, `${cwd}/dist/packages`))
+      )
+  }
+)
+
+gulp.task(
+  'copyicon',
+  () => {
+    return gulp.src(['./packages/**/icon/*.*'])
+      .pipe(
+        gulp.dest(process.cwd().replace(cwd, `${cwd}/dist/packages`))
+      )
+  }
+)
+
+gulp.task(
+  'packages',
+  () => {
+    return gulp.src(['./packages/**/*.ts'])
       .pipe(
         ts(
           require('../../tsconfig.json')
         )
       )
       .pipe(
-        gulp.dest(process.cwd())
+        gulp.dest(process.cwd().replace(cwd, `${cwd}/dist/packages`))
       )
   }
 )
 
+gulp.task(
+  'utils',
+  () => {
+    return gulp.src(['./utils/*.ts', './utils/*.js'])
+      .pipe(
+        ts(
+          require('../../tsconfig.json')
+        )
+      )
+      .pipe(
+        gulp.dest(process.cwd().replace(cwd, `${cwd}/dist/utils`))
+      )
+  }
+)
+
+gulp.task(
+  'main',
+  () => {
+    return gulp.src(['*.ts'])
+      .pipe(
+        ts(
+          require('../../tsconfig.json')
+        )
+      )
+      .pipe(
+        gulp.dest(process.cwd().replace(cwd, `${cwd}/dist`))
+      )
+  }
+)
+
+
 rimraf.sync(path.join(cwd, 'dist'))
 
-gulp.task('default', ['vue'])
+gulp.task('default', ['copy', 'copyvue', 'packages', 'utils', 'main'])
